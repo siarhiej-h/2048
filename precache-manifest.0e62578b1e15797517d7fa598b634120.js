f2e98e70e2444bb298e8b29d8b3a8297a376bed9
siarhiej-h <41705423+siarhiej-h@users.noreply.github.com>
2020-01-19 23:37:52 +0000

    Deploying to gh-pages from  3b7a8b9f5d0048e6f0ca2294a9b75b8bb9528635

---
+++ precache-manifest.0e62578b1e15797517d7fa598b634120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ea2794063761958d7982ff8f43a15b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "847c06e099e4f94285c7",
+    "url": "/static/css/main.1d17aedb.chunk.css"
+  },
+  {
+    "revision": "7846d6ddc35b81708f76",
+    "url": "/static/js/2.60dba9ed.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.60dba9ed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "847c06e099e4f94285c7",
+    "url": "/static/js/main.4ef2e73c.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
